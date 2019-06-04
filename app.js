0f3f56b6d1f3fbbe20dcc18764f5820f23debe7e
IDM.air <innanadm8@gmail.com>
2019-06-03 23:56:35 -0400

    hardened meat

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ function calc() {
         if (letter1 == 'L') loveCount += 2;
         if (letter1=='O') loveCount+=2;
         if (letter1=='V') loveCount+=2;
+        if (letter1=='E') loveCount+=2;
 
 
 
