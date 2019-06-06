a101d04058525455e1f7322716348065b4e8b7e4
IDM.air <innanadm8@gmail.com>
2019-06-05 20:52:31 -0400

    ERR

--- app.js
+++ app.js
@@ -17,31 +17,31 @@ function calc() {
     const loveCount = 0;
 
     // ?for loop?
-    for (count=0; count < firstLength; count++) {
-        letter1 = first.substring(count, count + 1);
+    var Count;
+    for (Count=0; count < firstLength; Count++) {
+        letter1 = first.substring(Count, Count + 1);
 
-        if (letter1 == 'L') loveCount += 1;
-        if (letter1=='O') loveCount+=3;
+        if (letter1=='L') loveCount += 2;
+        if (letter1=='O') loveCount+=2;
         if (letter1=='V') loveCount+=2;
-        if (letter1=='I') loveCount+=4;
-        if (letter1=='N') loveCount+=5;
-        if (letter1=='G') loveCount+=7;
-        if (letter1=='M') loveCount+=6;
-        if (letter1=='E') loveCount+=8;
+        if (letter1=='E') loveCount+=2;
+        if (letter1=='Y') loveCount+=3;
+        if (letter1=='O') loveCount+=1;
+        if (letter1=='U') loveCount+=3;
     }
 
     // ?another loop?
-    for (count=0; count < secondLength; count++) {
-        letter2 = second.substring(count, count+1);
+    var Count;
+    for (Count=0; Count < secondLength; Count++) {
+        letter2 = second.substring(Count, Count+1);
 
-        if (letter2 == 'L') loveCount += 1;
-        if (letter2=='O') loveCount+=3;
+        if (letter2 == 'L') loveCount += 2;
+        if (letter2=='O') loveCount+=2;
         if (letter2=='V') loveCount+=2;
-        if (letter2=='I') loveCount+=4;
-        if (letter2=='N') loveCount+=5;
-        if (letter2=='G') loveCount+=7;
-        if (letter2=='M') loveCount+=6;
-        if (letter2=='E') loveCount+=8;
+        if (letter2=='E') loveCount+=2;
+        if (letter2=='Y') loveCount+=3;
+        if (letter2=='O') loveCount+=1;
+        if (letter2=='U') loveCount+=3;
     }
 
     var amount = 0;
